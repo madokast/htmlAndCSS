be234104ca7314aeb0dfbf8a7b5e811c5bd85b58
madokast <578562554@qq.com>
2020-01-18 22:45:48 +0800

    起飞

---
+++ ToStructure/vueLearn/hello/04-模块化开发/模块化/aaa.js
@@ -0,0 +1,16 @@
+let name = 'xiaoming';
+let age = 18;
+let flag = true;
+
+const ADDRESS = '北京市';
+
+if(flag){
+  console.log(name);
+}
+
+export {
+  flag,
+  age
+};
+
+export default ADDRESS;
---
+++ ToStructure/vueLearn/hello/04-模块化开发/模块化/bbb.js
@@ -0,0 +1,13 @@
+import {flag} from "./aaa.js";
+
+if(flag){
+  console.log("小明是天才");
+}
+
+import ADDR from "./aaa.js"
+
+console.log(ADDR);
+
+import * as AAA from "./aaa.js";
+
+console.log(AAA.age);
