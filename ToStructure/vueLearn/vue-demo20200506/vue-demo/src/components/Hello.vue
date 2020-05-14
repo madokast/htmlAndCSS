02c64a4efb0d18d4363148bf0c6a92be5a24a9cd
madokast <578562554@qq.com>
2020-05-14 11:03:35 +0800

    vue learn

---
+++ ToStructure/vueLearn/vue-demo20200506/vue-demo/src/components/Hello.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="hello">
+    <h1>{{msg}}</h1>
+    <el-radio v-model="radio" label="1">选择1</el-radio>
+    <el-radio v-model="radio" label="2">选择2</el-radio>
+  </div>
+</template>
+
+<script>
+//http://localhost:25800/#/hello
+export default {
+  name: "hello",
+  data() {
+    return {
+      msg: "hello vue",
+      radio: "1"
+    };
+  }
+};
+</script>
+
+<style>
+.hello {
+  color: red;
+}
+</style>
---
+++ ToStructure/vueLearn/vue-demo20200506/vue-demo/src/components/MyTable.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <el-table :data="tableData">
+      <el-table-column prop="date" label="日期" width="140"></el-table-column>
+      <el-table-column prop="name" label="姓名" width="120"></el-table-column>
+      <el-table-column prop="address" label="地址"></el-table-column>
+    </el-table>
+  </div>
+</template>
+
+<script>
+export default {
+  //import 引入的组件需要注入到对象中才能使用
+  components: {},
+  props: {},
+  data() {
+    const item = {
+      date: "2016-05-02",
+      name: "王小虎",
+      address: "上海市普陀区金沙江路 1518 弄"
+    };
+    return {
+      tableData: Array(20).fill(item)
+    };
+  },
+  //计算属性
+  computed: {},
+  //监控data中数据变化
+  watch: {},
+  //方法
+  methods: {},
+  //声明周期 - 创建完成（可以访问当前this实例）
+  created() {},
+  //生命周期 - 挂载完成（可以访问DOM元素）
+  mounted() {},
+  beforeCreate() {}, //生命周期 - 创建之前
+  beforeMount() {}, //生命周期 - 挂载之前
+  beforeUpdate() {}, //声明周期 - 更新之前
+  updated() {}, //生命周期 - 更新之后
+  beforeDestroy() {}, //生命周期 - 销毁之前
+  destroyed() {}, //生命周期 - 销毁之后
+  activated() {} //缓存keep-alive
+};
+</script>
+
+<style scoped>
+</style>
